27bba0ce7c7f1ce72a840b8e70e45f3efba69f32
Nirmay Mehta <nirmay.paras.mehta@gmail.com>
2022-05-08 23:53:16 +0530

    Login and Register completed

---
+++ Sermo/Controllers/ConversationsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Sermo
+//
+//  Created by Nirmay Mehta on 07/05/22.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+    
+    
+
+
+}
+
---
+++ Sermo/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  Sermo
+//
+//  Created by Nirmay Mehta on 08/05/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    public var width: CGFloat {
+        return self.frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return self.frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    public var right: CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+    
+}
